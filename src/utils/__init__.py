ec277ca87953041f15b0c906ed27d010181a07e7
iinumac <iinumac@mac.com>
2026-01-11 16:09:12 +0900

    Export data_loader functions from utils module

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,1 +1,10 @@
 # ユーティリティモジュール
+from .data_loader import (
+    load_races,
+    load_results,
+    clear_cache,
+    get_horse_history,
+    get_jockey_stats,
+    get_trainer_stats,
+    search_horse_by_name,
+)
